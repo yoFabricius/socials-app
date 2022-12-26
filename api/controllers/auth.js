8759491b1b0d4dd55df4cd8b69dba145cac65cf4
[Fabricius Eusebio] <yofabricius@icloud.com>
2022-12-26 13:34:10 -0500

    lets get back to learning

---
+++ api/controllers/auth.js
@@ -0,0 +1,23 @@
+import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
+
+export const register = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("user alredy exist");
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)";
+    const values = [req.body.username, req.body.email, hashedPassword, req.body.name];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created");
+    });
+  });
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
---
+++ api/index.js
@@ -0,0 +1,21 @@
+import  express  from "express";
+const app = express()
+
+import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+
+
+app.listen(8800, ()=>{
+    console.log("API WORKING DAWG")
+})
