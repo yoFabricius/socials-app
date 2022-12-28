0727bb83c9ab323074ea0c53c73da026b5c18a17
[Fabricius Eusebio] <yofabricius@icloud.com>
2022-12-27 21:01:21 -0500

    yess sirr

--- api/index.js
+++ api/index.js
@@ -6,8 +6,13 @@ import authRoutes from "./routes/auth.js"
 import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
+// middlewares
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
